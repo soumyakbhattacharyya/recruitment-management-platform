672e5e78676a9928615dd678a8802703227e53d2
soumyakbhattacharyya <bhattacharyya.soumyak@gmail.com>
2020-09-14 09:13:53 +0530

    BDT setup completes

--- src/main/java/recruiter/opening/RecruiterApplication.java
+++ src/main/java/recruiter/opening/RecruiterApplication.java
@@ -3,7 +3,7 @@ package recruiter.opening;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class RecruiterApplication {
 
 	public static void main(String[] args) {
